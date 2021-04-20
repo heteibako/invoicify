fb621fca1959d551bf0c6b4d95d0e61354919eb3
heteibako <hetei_bako@yahoo.com>
2021-04-20 12:45:55 +0200

    added register route

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
@@ -7,8 +10,64 @@ const UserSchema = new Schema({
   invoices: [{ type: mongoose.Types.ObjectId, ref: 'Invoice' }],
   createdAt: { type: Date, default: Date.now },
   company: { type: mongoose.Types.ObjectId, ref: 'Company' },
-  password: { type: String, required: [true, 'Please add password'] },
-  email: { type: String, required: [true, 'Please add email'] },
+  password: { type: String, minlength: 6, select: false, required: [true, 'Please add password'] },
+  email: { type: String, unique: true, required: [true, 'Please add email'] },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpire: {
+    type: Date,
+  },
+  phone: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+    required: false,
+    default: 'placeholder.jpg',
+  },
+  isConsent: {
+    type: Boolean,
+    required: [true, 'Please accept the therms and comditions'],
+  },
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  // eslint-disable-next-line no-undef
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    // eslint-disable-next-line no-undef
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//GEnerate and hash password token
+
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 module.exports = User = mongoose.model('User', UserSchema);
---
+++ pages/api/register/index.js
@@ -0,0 +1,54 @@
+import connectDB from '@config/db';
+import User from '@models/User';
+
+connectDB();
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { firstName, lastName, password, email, isConsent, phone, company } = req.body;
+      const user = await User.create({
+        firstName,
+        lastName,
+        password,
+        email,
+        isConsent,
+        phone,
+        company,
+      });
+      sendTokenResponse(user, 200, res);
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.json({
+    success: true,
+    token,
+  });
+};
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+};
