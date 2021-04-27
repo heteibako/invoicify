24337a81d98670d087a528c185987c90d42821ba
heteibako <hetei_bako@yahoo.com>
2021-04-27 13:00:55 +0200

    extended invoice model

--- models/Invoice.js
+++ models/Invoice.js
@@ -3,8 +3,16 @@ const { Schema } = mongoose;
 
 const InvoiceSchema = new Schema({
   title: String,
+  invoiceNumber: String,
+  logo: String,
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
+  dueDate: { type: Date, default: Date.now },
+  paymentTerm: String,
+  billTo: String,
+  shipTo: String,
+  notes: String,
+  terms: String,
   invoiceFor: {
     name: String,
     address: {
@@ -13,9 +21,12 @@ const InvoiceSchema = new Schema({
       postCode: { type: String },
     },
   },
+  items: [],
   sum: { type: Number },
-  taxes: { type: Number },
-  paid: { type: Boolean },
+  tax: { type: Number },
+  amountPaid: { type: Number },
+  subTotal: { type: Number },
+  balance: { type: Number },
 });
 
 module.exports = mongoose.models.Invoice || mongoose.model('Invoice', InvoiceSchema);
